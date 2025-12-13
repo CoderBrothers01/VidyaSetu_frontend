9dd3571f6d03131db3fd9ca347938c02d7f841ed
Lakshitdevjani <lakshitdevjani4@email.com>
2025-12-13 17:51:22 +0530

    'push'

--- src/screens/bottom/Admin/Attendance/StaffAttendance.jsx
+++ src/screens/bottom/Admin/Attendance/StaffAttendance.jsx
@@ -1,12 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import Colors from '../../../../utils/Colors';
+import Stars from '../../../../components/CustomStars';
+import CustomHeader from '../../../../components/CustomHeader';
+import globalStyle from '../../../../utils/globalStyle';
+import { useNavigation } from '@react-navigation/native';
 
 const StaffAttendance = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>StaffAttendance</Text>
-    </View>
-  )
-}
+    <LinearGradient
+      colors={[Colors.studentBackground1, Colors.studentBackground2]}
+      start={{ x: 0, y: 0.5 }}
+      end={{ x: 1, y: 0.5 }}
+      style={{ flex: 1 }}
+    >
+      <Stars />
+      <CustomHeader
+        title="Staff Attendance"
+        onBackPress={() => {
+          navigation.goBack();
+        }}
+      />
+    </LinearGradient>
+  );
+};
 
-export default StaffAttendance
+export default StaffAttendance;
